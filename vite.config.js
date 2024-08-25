0189d442f8b51d7f74a055b012d789bba3b72d89
John Dave Dalmao <johndavedalmao@gmail.com>
2024-08-25 15:39:30 +0800

    base code for portfoliooo-v2

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import rehypePrism from '@mapbox/rehype-prism';
+import mdx from '@mdx-js/rollup';
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from '@remix-run/dev';
+import rehypeImgSize from 'rehype-img-size';
+import rehypeSlug from 'rehype-slug';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
+
+export default defineConfig({
+  assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
+  build: {
+    assetsInlineLimit: 1024,
+  },
+  server: {
+    port: 7777,
+  },
+  plugins: [
+    mdx({
+      rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      providerImportSource: '@mdx-js/react',
+    }),
+    remixCloudflareDevProxy(),
+    remix({
+      routes(defineRoutes) {
+        return defineRoutes(route => {
+          route('/', 'routes/home/route.js', { index: true });
+        });
+      },
+    }),
+    jsconfigPaths(),
+  ],
+});
